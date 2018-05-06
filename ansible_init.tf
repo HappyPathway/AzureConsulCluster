09e9c22a2ebc03cbf6f072d56904931cd3c7b036
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-05 22:37:42 -0700

    splitting out ansible setup into separate null_resource

--- ansible_init.tf
+++ ansible_init.tf
@@ -1,11 +1,9 @@
-resource "null_resource" "datadog" {
-  count = "${var.datadog_monitor ? var.count : 0}"
+resource "null_resource" "ansible_init" {
+  count = "${var.count}"
   # Changes to any instance of the cluster requires re-provisioning
   triggers {
     cluster_instance_ids = "${join(",", azurerm_virtual_machine.avm.*.id)}"
   }
-
-
 
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
@@ -15,12 +13,17 @@ resource "null_resource" "datadog" {
       user     = "${var.system_user}"
       password = "${var.system_password}"
   }
+  
+  provisioner "file" {
+      source = "${path.module}/files/requirements.txt"
+      destination = "/tmp/requirements.txt"
+  }
 
   provisioner "remote-exec" {
-    inline = [
-      "curl ${var.ddog_install_script} | sudo DD_API_KEY=${var.datadog_key} bash",
-      "sudo ansible-playbook /tmp/playbooks/datadog_agent.yaml -e datadog_api_key=${var.datadog_key} -e service_name=${var.service_name}",
-      "rm -rf /tmp/playbooks"
-    ]
+      inline = [
+          "sudo apt-get update",
+          "sudo apt-get install -y python-pip",
+          "sudo pip install -r /tmp/requirements.txt"
+      ]
   }
 }
