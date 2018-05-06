8a6c49852a668fa3739911511e9250753b8a6755
Dave Arnold <darnold@l-sfh3k1htdd-m.gapinc.gap.com>
2018-05-05 21:05:54 -0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "location" {
+    default = "West US 2"
+}
+
+variable "count" {
+    default = 3
+}
+
+variable "disk_size" {
+    default = 1024
+}
+
+variable "system_user" {
+    default = "admin"
+}
+
+variable "system_password" {
+    default = "admin"
+}
+
+variable "datadog_key" {
+    default = 0
+}
+
+variable "ddog_install_script" {
+    default = "https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh"
+}
+
+variable "datadog_monitor" {
+    default = true
+}
+
+variable "consul_cluster" {
+    default = "consul-westus"
+} 
+
+variable "azure_subscription" {} 
+variable "azure_tenant" {}
+variable "azure_client" {} 
+variable "secret_access_key" {}
+variable "azure_secret" {}
+
+variable "service_name" {
+    default = "consul-cluster"
+}
